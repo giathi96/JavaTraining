d636209f23dd9edbcd83717e80d038b3c7110fcd
giathi96 <56151578+giathi96@users.noreply.github.com>
2021-10-27 15:04:16 +0700

    Add files via upload

---
+++ JavaTest.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class JavaTest{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so can kiem tra: ");
+        int number = sc.nextInt();
+        if(checkNumber(number)){
+            System.out.println(number + " la mot so dep!");
+        } else {
+            System.out.println(number + " khong phai la mot so dep!");
+        }
+    }
+
+    public static Boolean checkNumber (int n) {
+        int item = 0;
+        Set<Integer> setA = new HashSet<Integer>();
+        while (n > 0){
+            item = n % 10;
+            if(setA.contains(item)){
+                return false;
+            } else {
+                setA.add(item);
+                n /= 10;
+            }
+        }
+        return true;
+    }
+}
